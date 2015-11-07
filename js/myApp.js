8e6134ca016c6c78348050df3c4c2bc779efd653
lijun <lijunboy00@163.com>
2015-11-07 16:43:08 +0800

    学习angular base 1

---
+++ js/myApp.js
@@ -0,0 +1,7 @@
+/**
+ *
+ * Created by Administrator on 2015/11/7.
+ */
+"use strict"
+
+var app = angular.module("myApp",[]);
---
+++ js/myCtro.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * Created by Administrator on 2015/11/7.
+ */
+"use strict"
+app.controller("personController", function ($scope) {
+    $scope.names =[{name:"张三",age:"112",ename:"Glutton"},{name:"李四",age:"123",ename:"GluttonMan"}];
+});
